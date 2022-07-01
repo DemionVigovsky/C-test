25886233760103dd52dd7a47c84092170cda79b9
DemionVigovsky <morrow130790130@gmail.com>
2022-07-01 17:15:42 +0300

    Добавили популярные имена

---
+++ Examples/Example005_PopularName/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "сара")
+{
+    Console.WriteLine("Ура, это же моя Сарочка!");
+}
+else
+{
+    Console.Write("А, это ты ");
+    Console.WriteLine(username);
+}
