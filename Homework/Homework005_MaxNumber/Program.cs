8cabcfe05b9dbc5a38c2d71cc23c3f0ebf733510
DemionVigovsky <morrow130790130@gmail.com>
2022-07-02 17:40:08 +0300

    Готово

---
+++ Homework/Homework005_MaxNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB && numberA > numberC)
+    Console.WriteLine("Максимальное число " + numberA);
+if(numberB > numberA && numberB > numberC)
+    Console.WriteLine("Максимальное число " + numberB);
+if(numberC > numberA && numberC > numberB)
+    Console.WriteLine("Максимальное число " + numberC);
