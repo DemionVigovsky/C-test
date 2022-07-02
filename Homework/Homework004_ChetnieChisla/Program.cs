0a538b7a57de5b38cbae8b155fc9cf16feb1fb41
DemionVigovsky <morrow130790130@gmail.com>
2022-07-02 17:39:38 +0300

    Готово

---
+++ Homework/Homework004_ChetnieChisla/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача №6
+
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 0)
+    Console.WriteLine(numberA + " - Четное");
+    else 
+        Console.WriteLine(numberA + " - Не четное");
