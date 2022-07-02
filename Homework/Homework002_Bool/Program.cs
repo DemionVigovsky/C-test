0a581c13a6140096da7f57f800bb9d3e8b0e8bbf
DemionVigovsky <morrow130790130@gmail.com>
2022-07-02 16:05:05 +0300

    Квадратные числа

---
+++ Homework/Homework002_Bool/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA == numberB * numberB) {
+    Console.WriteLine("Да");
+} else {
+    Console.WriteLine ("Неееет");
+}
