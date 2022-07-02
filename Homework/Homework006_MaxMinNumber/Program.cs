7d607111d8b1b1039785b5469b27c85bec26f65b
DemionVigovsky <morrow130790130@gmail.com>
2022-07-02 18:26:23 +0300

    Готово

---
+++ Homework/Homework006_MaxMinNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+    Console.WriteLine ("Max = " + numberA);
+if(numberB > numberA)
+    Console.WriteLine ("Max = " + numberB);
